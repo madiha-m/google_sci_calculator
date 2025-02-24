6bb6f1d71ed02f147b5abf643a606df33d8ff92a
madiha-m <madiha.muhammad.dev@gmail.com>
2025-02-25 04:24:23 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Calculator from "./components/Calculator";
-import { BASE_UI, panelClass } from "./utils/styles";
+import { panelClass } from "./utils/styles";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className={panelClass}>
       <Calculator />
